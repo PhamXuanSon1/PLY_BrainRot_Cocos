771e83b6fc1b743ff4bb5fa1181ea7b42f311943
PhamXuanSon1 <sonpxhe171548@fpt.edu.vn>
2026-09-07 14:41:58 +0700

    Demon

--- assets/7.Scripts/Tool/GameController.ts
+++ assets/7.Scripts/Tool/GameController.ts
@@ -24,8 +24,8 @@ export class GameController extends Component {
 
   start() {
     playableHelper.gameStart();
-    const androidUrl = "https://play.google.com/store/apps/details?id=com.brainrott.minigame.challenge";
-    const iosUrl = "";
+    const androidUrl = "";
+    const iosUrl = "https://apps.apple.com/us/app/tip-tap-challenge-minigame/id6760379229";
     playableHelper.setStoreUrl(iosUrl, androidUrl); // this section only needs for Google and Unity channel
   }
 
